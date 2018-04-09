c48af9af1d06ac31f004f11d165c174321ea1f0e
Hannah Weber <hkwweber@gmail.com>
2018-04-09 19:52:44 -0400

    solve find closest val in BST problem

---
+++ binary-search-trees.js
@@ -0,0 +1,21 @@
+/* find closest value in BST
+given BST data structure consisting of BST nodes. Each node has value (integer), and left and right (children). a node is a BST only if its value is greater than the vals of every node to its left, its value is less than or equal to the values of every node to its right, and both children nodes are either BST nodes or null values. also given target integer. write a func that finds the closest value to that target value. assume there will only be one closest value
+
+*/
+
+// first attempt:
+function findClosestValueInBst(tree, target) {
+	let closest = { val: null, diff: Infinity };
+
+	//helper function to recurse
+	const recurser = curr => {
+		if (curr.left) recurser(curr.left);
+		let currDiff = Math.abs(curr.value - target);
+		if (currDiff < closest.diff) {
+			closest = { val: curr.value, diff: currDiff };
+		}
+		if (curr.right) recurser(curr.right);
+	};
+	recurser(tree);
+	return closest.val;
+}
