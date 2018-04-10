2f491fd0b7e4f21c0462c9f8e17d7a5235a19cb0
Hannah Weber <hkwweber@gmail.com>
2018-04-10 13:13:31 -0400

    time and space complexity of bst search

--- binary-search-trees.js
+++ binary-search-trees.js
@@ -1,6 +1,12 @@
-/* find closest value in BST***************************************************
+/*
+ find closest value in BST***************************************************
 given BST data structure consisting of BST nodes. Each node has value (integer), and left and right (children). a node is a BST only if its value is greater than the vals of every node to its left, its value is less than or equal to the values of every node to its right, and both children nodes are either BST nodes or null values. also given target integer. write a func that finds the closest value to that target value. assume there will only be one closest value
 
+
+avg time complexity - o log n
+worst time complexity - o n  (if each node only has one branch - must look at every node)
+recursive function - avg space complexity - o log n - adding n frames on call stack. worst case is o n
+iteratively - time complexity is the same but space complexity is only o(1) since we're not adding frames to the callstack, only storing and changing a variable
 */
 
 // first attempt:
@@ -24,12 +30,13 @@ function findClosestValueInBst(tree, target) {
 
 //with a while loop:
 function findClosestValueInBst(tree, target) {
-  let closest = {val: null, diff: Infinity};
+  let closest = Infinity;
 	let curr = tree;
 	while (curr) {
 		let currDiff = Math.abs(curr.value - target);
-		if (currDiff < closest.diff) {
-			closest = {val: curr.value, diff: currDiff};
+		let closestDiff = Math.abs(closest - target);
+		if (currDiff < closestDiff) {
+			closest = curr.value;
 		}
 		if (target < curr.value) curr = curr.left;
 		else if (target > curr.value) curr = curr.right;
