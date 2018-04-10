bd702addb9c2d3ff79edea308f3a42d9610b82e3
Hannah Weber <hkwweber@gmail.com>
2018-04-10 13:06:55 -0400

    bst update

--- binary-search-trees.js
+++ binary-search-trees.js
@@ -1,24 +1,25 @@
-/* find closest value in BST
+/* find closest value in BST***************************************************
 given BST data structure consisting of BST nodes. Each node has value (integer), and left and right (children). a node is a BST only if its value is greater than the vals of every node to its left, its value is less than or equal to the values of every node to its right, and both children nodes are either BST nodes or null values. also given target integer. write a func that finds the closest value to that target value. assume there will only be one closest value
 
 */
 
 // first attempt:
 function findClosestValueInBst(tree, target) {
-  let closest = { val: null, diff: Infinity };
+  let closest = Infinity;
 
 	//helper function to recurse
 	const recurser = curr => {
 		if (!curr) return;
 		let currDiff = Math.abs(curr.value - target);
-		if (currDiff < closest.diff) {
-			closest = { val: curr.value, diff: currDiff };
+		let closestDiff = Math.abs(closest - target);
+		if (currDiff < closestDiff) {
+			closest = curr.value;
 		}
-		if (target < curr.value) return recurser(curr.left);
-		if (target > curr.value) return recurser(curr.right);
+		if (target < curr.value) recurser(curr.left);
+		if (target > curr.value) recurser(curr.right);
 	};
 	recurser(tree);
-	return closest.val;
+	return closest;
 }
 
 //with a while loop:
@@ -36,3 +37,13 @@ function findClosestValueInBst(tree, target) {
 	}
 	return closest.val;
 }
+
+
+/*  BST CONSTRUCTION *********************************************************
+write a BST class
+should have value prop, left and right props should point to null or another BST
+rules: its value is greater than the values of every node to its left. its value is less than or equal to the values of every node to its right.
+should support insertion, searching, and removal of values
+removal method should only remove the first instance of the target value
+
+*/
