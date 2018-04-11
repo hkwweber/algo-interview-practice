5dde8f6b57c6b6fd21aab45a85316a33c3e2be7b
Hannah Weber <hkwweber@gmail.com>
2018-04-11 14:17:38 -0400

    bst remove

--- binary-search-trees.js
+++ binary-search-trees.js
@@ -11,7 +11,7 @@ iteratively - time complexity is the same but space complexity is only o(1) sinc
 
 // first attempt:
 function findClosestValueInBst(tree, target) {
-  let closest = Infinity;
+	let closest = Infinity;
 
 	//helper function to recurse
 	const recurser = curr => {
@@ -30,21 +30,18 @@ function findClosestValueInBst(tree, target) {
 
 //with a while loop:
 function findClosestValueInBst(tree, target) {
-  let closest = Infinity;
+	let closest = Infinity;
 	let curr = tree;
 	while (curr) {
-		let currDiff = Math.abs(curr.value - target);
-		let closestDiff = Math.abs(closest - target);
-		if (currDiff < closestDiff) {
+		if (Math.abs(curr.value - target) < Math.abs(closest - target)) {
 			closest = curr.value;
 		}
 		if (target < curr.value) curr = curr.left;
 		else if (target > curr.value) curr = curr.right;
 		else break;
 	}
-	return closest.val;
+	return closest;
 }
-
 
 /*  BST CONSTRUCTION *********************************************************
 write a BST class
@@ -53,4 +50,76 @@ rules: its value is greater than the values of every node to its left. its value
 should support insertion, searching, and removal of values
 removal method should only remove the first instance of the target value
 
+
+
+removal -- grab smallest val in right subtree of the node you want to remove
+replace node to remove with that smallest val from right subtree
+remove that smallest val node
+
 */
+
+class BST {
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
+
+	insert(value) {
+		let currTree = this;
+		let direction = value < currTree.value ? "left" : "right";
+		if (currTree[direction]) currTree[direction].insert(value);
+		else currTree[direction] = new BST(value);
+		return this;
+	}
+
+	contains(value) {
+		let currTree = this;
+		if (currTree.value === value) return true;
+		let direction = value < currTree.value ? "left" : "right";
+		if (currTree[direction]) return currTree[direction].contains(value);
+		return false;
+	}
+
+//passed all but two edge cases for my remove solution, so this is algo's remove solution below
+	remove(value, parent = null) {
+		if (value < this.value) {
+			if (this.left) {
+				this.left.remove(value, this);
+			}
+		} else if (value > this.value) {
+			if (this.right) {
+				this.right.remove(value, this);
+			}
+		} else {
+			if (this.left && this.right) {
+				this.value = this.right.findSmallestValue();
+				this.right.remove(this.value, this);
+			} else if (!parent) {
+				if (this.left) {
+					this.value = this.left.value;
+					this.right = this.left.right;
+					this.left = this.left.left;
+				} else if (this.right) {
+					this.value = this.right.value;
+					this.left = this.right.left;
+					this.right = this.right.right;
+				} else this.value = null;
+			} else if (parent.left === this) {
+				parent.left = this.left ? this.left : this.right;
+			} else if (parent.right === this) {
+				parent.right = this.left ? this.left : this.right;
+			}
+		}
+		return this;
+	}
+
+	findSmallestValue() {
+		let currTree = this;
+		while (currTree) {
+			if (currTree.left) currTree = currTree.left;
+			else break;
+		}
+		return currTree.value;
+	}
+}
