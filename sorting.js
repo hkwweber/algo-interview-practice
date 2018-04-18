8ee7ba823ec00966909e6ed1b8490eca80c30ce9
Hannah Weber <hkwweber@gmail.com>
2018-04-18 11:06:57 -0400

    begin sorting

---
+++ sorting.js
@@ -0,0 +1,7 @@
+/*BUBBLE SORT ****************************************************************
+takes an array and returns a sorted version. use the bubble sort algo
+bubble sort: repeatedly steps through the list, compares each pair of adjacent items and swaps them if they're in the wrong order. the pass through the list is repeated until no swaps are needed
+named for the way smaller or larger elements 'bubble' to the top of the list
+slow sort, but can be practical if the input is in mostly sorted order with some out of order elements nearly in position
+*/
+
