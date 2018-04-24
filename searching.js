fac3d7633c32767c5adcb3ce78fafb04aa5d30fa
Hannah Weber <hkwweber@gmail.com>
2018-04-24 17:12:06 -0400

    binary search iterative solution

--- searching.js
+++ searching.js
@@ -1,8 +1,9 @@
 /* BINARY SEARCH ********************************************************
 takes in a sorted array of integers as well as a target integer. function should use binary search algo to find if the target number is contained in the array. return index or -1 if not within array
 */
-//O(log n) time and space complexity
 
+
+//recursive solution: O(log n) time and space complexity
 function binarySearch(array, target, startIdx = 0, endIdx = array.length-1) {
 	//base case
 	if (endIdx === startIdx) {
@@ -20,4 +21,22 @@ function binarySearch(array, target, startIdx = 0, endIdx = array.length-1) {
 		//look at array to the left of split point
 		return binarySearch(array, target, startIdx, splitIdx-1);
 	}
+}
+
+//iterative solution: still O(log n) time but down to constant space w/o recursion
+function binarySearch(array, target) {
+	let startIdx = 0;
+	let endIdx = array.length-1;
+	while (endIdx !== startIdx) {
+		let splitIdx = Math.floor((endIdx + startIdx)/2);
+		let splitPoint = array[splitIdx];
+		if (splitPoint === target) return splitIdx;
+		if (splitPoint < target) {
+			startIdx = splitIdx +1;
+		}
+		else if (splitPoint > target) {
+			endIdx = splitIdx -1;
+		}
+	}
+	return array[startIdx] === target ? startIdx : -1;
 }
